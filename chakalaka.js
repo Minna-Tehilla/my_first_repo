e15aeb84f71618c4596af884fd54d2b274910eda
mina <mt@gfdg.com>
2023-01-23 00:16:21 +0200

    add js

--- chakalaka.js
+++ chakalaka.js
@@ -1,18 +1,23 @@
 let flag;
 function changeBG() {
    if (flag) {
-      document.body.style.background = "radial-gradient(circle, rgba(246,4,8,1) 0%, rgba(244,237,237,1) 100%)";
-      document.body.style.background = 'rgb(246,4,8)';
+      document.body.style.background = "linear-gradient(90deg, rgba(242,22,6,1) 0%, rgba(183,184,242,1) 50%, rgba(19,6,241,1) 100%)";
    }
-   else { document.body.style.backgroundColor = "blue" }
+   else { document.body.style.background = "linear-gradient(90deg, rgba(19,6,241,1) 0%, rgba(183,184,242,1) 50%, rgba(242,22,6,1) 100%)"; }
    flag = !flag;
 }
 
 function playChakalaka() {
-   const intervalID = setInterval(changeBG, 500);
+   document.getElementById("pid").style.display="none"
+      document.getElementById("manpic").style.display="block"
+      document.getElementById("divid").innerHTML="Be quiet already!!!"
+   const intervalID = setInterval(changeBG, 300);
    setTimeout(() => {
       clearInterval(intervalID);
-   }, 10000);
+      document.getElementById("pid").style.display="block"
+      document.getElementById("manpic").style.display="none"
+      document.getElementById("divid").innerHTML="Great Job, class!!!"
+   }, 5000);
 }
 
 
